2c5da2aff70efd2e03352c150ef071594ed36b38
Koh-iiii <f.s.ensemble@gmail.com>
2025-04-13 20:10:55 +0900

    メニューと予約のシリアライザの作成・対応するビューの作成

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -2,6 +2,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('menu/', views.MenuItemView.as_view(), name='menu'),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='singlemenuitem'),
 ]
